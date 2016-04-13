646d6cd89fbfc1bee8cb10454843be6a2a0bf738
Royi Reshef <rreshef@cisco.com>
2016-04-13 16:16:24 +0300

    change name for python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geocity',
+      version='0.1',
+      description='offline geo city data',
+      url='https://github.com/royi1000/pycity',
+      author='Royi Reshef',
+      author_email='roy.myapp@gmail.com',
+      license='MIT',
+      packages=['geocity'],
+      zip_safe=False)
